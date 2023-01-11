8cab591a26f99b7485ea1776b3784b1304a5cb93
ElMehdiAitAzarine <96500013+ElMehdiAitAzarine@users.noreply.github.com>
2023-01-11 20:14:03 +0100

    Add files via upload

---
+++ Flask articles app/data.py
@@ -0,0 +1,15 @@
+def Articles():
+    articles = [
+        {'id':1,'title':'Article_one','body':' SQL Synthèse de cours et exercices Frédéric Brouard, Christian Soutou, Rudi Bruchez, Pearson Education, 2012 Bases de données de la modélisation au',
+        'author':'âataa llah',
+         'creat_date':'16_11_2022'},
+         
+        {'id':2,'title':'Article_twoo','body':' SQL Synthèse de cours et exercices Frédéric Brouard, Christian Soutou, Rudi Bruchez, Pearson Education, 2012 Bases de données de la modélisation au',
+        'author':'aymen tazarine',
+         'creat_date':'16_11_2022'},
+        
+        {'id':3,'title':'Article_three','body':' SQL Synthèse de cours et exercices Frédéric Brouard, Christian Soutou, Rudi Bruchez, Pearson Education, 2012 Bases de données de la modélisation au',
+        'author':'âabdoo tazarine',
+         'creat_date':'16_11_2022'}
+    ]
+    return articles
